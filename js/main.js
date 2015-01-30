acb0cb9d2286cebe2fd0ae68e165e847bb842dc8
haoguan <haohowardguan@gmail.com>
2015-01-30 12:30:40 +0000

    added cloudbase io support, still need animation for feedback

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var helper = new CBHelper("zypper-landing-page", "5e313b752f970c957931b916fb03f87e", new GenericHelper());
+helper.setPassword(hex_md5("haowu123"));
+
+jQuery(document).ready(function($) {
+
+  $('.email_header_submit').on("click", function(){
+    var email = $('.email_header').val()
+    insertEmail(email)
+  });
+
+  $('.email_footer_submit').on("click", function(){
+    var email = $('.email_footer').val()
+    insertEmail(email)
+  });
+});
+
+function insertEmail(email) {
+  if (email) {
+    var dataObject = { "email" : email };
+    helper.insertDocument("emails", dataObject, null, function(resp) {
+      // update animation for front end
+    });
+  }
+}
