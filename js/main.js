a7377e957702580d7992056933b7cb134ee450d2
jonwu <jonwu92@gmail.com>
2015-02-03 15:31:55 -0800

    update frontend

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 // initialise the helper object with the code, secret code and the
 // generic helper
+// 
+
+
 var helper = new CBHelper("zypper-landing-page", "5e313b752f970c957931b916fb03f87e", new GenericHelper());
 // use the md5 library provided to set the password
 helper.setPassword(hex_md5("haowu123"));
@@ -7,11 +10,13 @@ helper.setPassword(hex_md5("haowu123"));
 jQuery(document).ready(function($) {
 
   $('.email_header_submit').on("click", function(){
+    
     var email = $('.email_header').val()
     insertEmail(email)
   });
 
   $('.email_footer_submit').on("click", function(){
+    
     var email = $('.email_footer').val()
     insertEmail(email)
   });
@@ -23,8 +28,13 @@ function insertEmail(email) {
     var dataObject = { "email" : email };
     helper.insertDocument("emails", dataObject, null, function(resp) {
       if (resp["outputData"] == "Inserted") {
-        alert("Thank you for registering!")
+        // $('.join').fadeOut('1500', function() {
+          
+        // });
+          alert("Thanks for joining!");
+        
       }
+
     });
   }
 }
